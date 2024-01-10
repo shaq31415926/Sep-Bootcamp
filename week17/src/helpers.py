350c33554b68f9502cd1b9bd75f577b6167b21da
shaq31415926 <sarah.haq11@gmail.com>
2024-01-10 12:13:04 +0100

    connect and load to s3

--- week17/src/helpers.py
+++ week17/src/helpers.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import boto3
+from io import StringIO 
+
 
 
 def identify_missing_data(df):
@@ -26,3 +29,29 @@ def identify_missing_data(df):
     missing_value_df.percent_missing = round(missing_value_df.percent_missing*100, 2) # format the percent_missing
     
     return missing_value_df[missing_value_df.percent_missing > 0]
+
+
+def connect_to_s3(aws_access_key_id, aws_secret_access_key):
+    """Methods that connects to s3"""
+
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+
+    print("Connection to s3 made")
+
+    return s3_client
+
+
+def load_df_to_s3(df, aws_access_key_id, aws_secret_access_key):
+    """Function that writes a data frame as a .csv file to a s3 bucket"""
+    
+    s3_client = connect_to_s3(aws_access_key_id, aws_secret_access_key)
+    
+    csv_buffer = StringIO() # create buffer to temporarily store the Data Frame
+    df.to_csv(csv_buffer, index=False) # code to write the data frame as csv file
+    response = s3_client.put_object(
+            Bucket=AWS_S3_BUCKET, Key=key, Body=csv_buffer.getvalue()
+        ) # this code writes the temp stored csv file and writes to s3
