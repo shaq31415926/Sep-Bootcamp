890343f2fb4f3fb5d2f0c62f4e087a9cbd7db06e
shaq31415926 <sarah.haq11@gmail.com>
2024-01-22 12:27:03 +0100

    code from class

--- week19/main.py
+++ week19/main.py
@@ -1,9 +1,12 @@
-import os
+from datetime import datetime
 from src.extract import extract_transactional_data
 from src.transform import identify_and_remove_duplicates
 from src.load_data_to_s3 import df_to_s3
 
+start_time = datetime.now()
+
 # this library is being used to read from the .env file
+import os
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,17 +16,24 @@ port = os.getenv("port")
 host = os.getenv("host")
 user_name = os.getenv("user")
 password = os.getenv("password")
-
-# step 1: extract and transform the data
-ot_transformed = extract_transactional_data(dbname, host, port, user_name, password)
-
-# step 2: identify and remove duplicate
-ot_wout_duplicates = identify_and_remove_duplicates(ot_transformed)
-
-# step 3: load data to s3
+# reading the variables that will connect to s3
 key="transformations_final/sh_online_trans_transformed.pkl"
 s3_bucket="sep-bootcamp"
 aws_access_key_id=os.getenv("aws_access_key_id")
 aws_secret_access_key=os.getenv("aws_secret_access_key_id")
 
+
+# step 1: extract and transform the data
+print("\nExtracting the data from redshift...")
+ot_transformed = extract_transactional_data(dbname, host, port, user_name, password)
+
+# step 2: identify and remove duplicate
+print("\nIdentifying and removing duplicates")
+ot_wout_duplicates = identify_and_remove_duplicates(ot_transformed)
+
+# step 3: load data to s3
+print("\nLoading data to s3 bucket")
 df_to_s3(ot_wout_duplicates, key, s3_bucket, aws_access_key_id, aws_secret_access_key)
+
+execution_time = datetime.now() - start_time
+print(f"Total execution time (hh:mm:ss) {execution_time}")
