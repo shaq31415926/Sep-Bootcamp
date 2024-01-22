df4e01f64c3ed11b7b833039b641098ce396b4e9
shaq31415926 <sarah.haq11@gmail.com>
2024-01-22 11:15:41 +0100

    code from class

--- week19/main.py
+++ week19/main.py
@@ -1,5 +1,7 @@
 import os
 from src.extract import extract_transactional_data
+from src.transform import identify_and_remove_duplicates
+from src.load_data_to_s3 import df_to_s3
 
 # this library is being used to read from the .env file
 from dotenv import load_dotenv
@@ -12,8 +14,16 @@ host = os.getenv("host")
 user_name = os.getenv("user")
 password = os.getenv("password")
 
-# step 1: extract data
-online_trans_transformed = extract_transactional_data(dbname, host, port, user_name, password)
+# step 1: extract and transform the data
+ot_transformed = extract_transactional_data(dbname, host, port, user_name, password)
 
-# step 2: transform data
+# step 2: identify and remove duplicate
+ot_wout_duplicates = identify_and_remove_duplicates(ot_transformed)
+
 # step 3: load data to s3
+key="transformations_final/sh_online_trans_transformed.pkl"
+s3_bucket="sep-bootcamp"
+aws_access_key_id=os.getenv("aws_access_key_id")
+aws_secret_access_key=os.getenv("aws_secret_access_key_id")
+
+df_to_s3(ot_wout_duplicates, key, s3_bucket, aws_access_key_id, aws_secret_access_key)
--- week19/src/transform.py
+++ week19/src/transform.py
@@ -1,1 +1,13 @@
-#TODO: Write a definition that identifies and removes duplicates
+def identify_and_remove_duplicates(df):
+    """Function that identifies and removes duplicated rows of data"""
+
+    if df.duplicated().sum() > 0:
+        df_cleaned = df.drop_duplicates(keep='first')
+
+        print("The shape of the data frame after removing duplicates is", df_cleaned.shape)
+    else:
+        df_cleaned = df
+        print("This data frame contains no duplicates")
+
+    return df_cleaned
+
